83a888d0b706ba3450e69b07dce98d716df9b40a
DhruvMalviya0 <dhruvnotfoundlol@gmail.com>
2025-06-28 19:31:29 +0530

    initialized-react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ChatInterface from './ChatInterface'
+import ChatInterface from './components/ChatInterface'
 function App() {
 
   return (
