cc013d12268e9d6fc9e06749aa868516f9c1b306
DhruvMalviya0 <dhruvnotfoundlol@gmail.com>
2025-06-28 19:20:47 +0530

    new main

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ChatInterface from './ChatInterface'
+function App() {
+
+  return (
+    <>
+      <ChatInterface/>
+    </>
+  )
+}
+
+export default App
